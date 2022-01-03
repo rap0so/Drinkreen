a54d577b96ccf4a051f766fcc0fdda9627a882a1
Rap0so <william.s.godoy53@gmail.com>
2022-01-02 22:51:59 -0300

    refactor(routes): rename product to beer

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'root#home'
   get '/home', to: 'root#home'
-  get '/product/:id', to: 'root#product'
+  get '/beer/new', to: 'root#new-beer'
+  get '/beer/:id', to: 'root#beer'
   get '/health', to: 'root#health'
-  # get '/home', to: 'root#home'
-  # root "articles#index"
+  post '/api/add', to: 'root#addBeer'
 end
